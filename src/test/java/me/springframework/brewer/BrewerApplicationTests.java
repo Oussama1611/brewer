d9aec34ca882c38ddbefa7c092bedc0796b36dfa
Oussama1611 <oussamakrab2000@gmail.com>
2023-02-14 09:39:10 +0100

    Initializing

---
+++ src/test/java/me/springframework/brewer/BrewerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.springframework.brewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
